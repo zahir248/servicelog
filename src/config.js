cf733fdefa19c5204a099d2beaae991ed627eb26
zahir248 <muhdzahir248@gmail.com>
2025-03-08 23:51:03 +0800

    update using production url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const BASE_API_URL = "http://localhost:8000/api";
+// const BASE_API_URL = "http://localhost:8000/api";
+
+const BASE_API_URL = "https://zahirdev-production.up.railway.app/api";
 
 export default BASE_API_URL;
